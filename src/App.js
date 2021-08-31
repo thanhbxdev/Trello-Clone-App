e67f684e24ed6e773d1a16875dd6c75627427a13
thanhbxdev <thanhbxdev@gmail.com>
2021-08-31 15:47:09 +0700

    Implement Drag and Drop,Eslint

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
-import React from "react";
-import './App.scss';
-import BoardBar from "./components/BoardBar/BoardBar";
-import AppBar from "./components/AppBar/AppBar";
-import BoardContent from "./components/BoardContent/BoardContent";
+import React from 'react'
+import './App.scss'
+import BoardBar from './components/BoardBar/BoardBar'
+import AppBar from './components/AppBar/AppBar'
+import BoardContent from './components/BoardContent/BoardContent'
 
 function App() {
-    return (
-        <div className="trello-master">
-            <AppBar/>
-            <BoardBar/>
-            <BoardContent/>
-        </div>
-    );
+  return (
+    <div className="trello-master">
+      <AppBar/>
+      <BoardBar/>
+      <BoardContent/>
+    </div>
+  )
 }
 
-export default App;
+export default App
--- src/components/BoardContent/BoardContent.js
+++ src/components/BoardContent/BoardContent.js
@@ -1,34 +1,53 @@
-import React, {useEffect, useState} from "react";
-import "./BoardContent.scss"
-import Column from "../Column/Column";
-import {initialData} from "../../actions/initialData";
-import {isEmpty} from "lodash";
-import {mapOrder} from "../../untilities/sort";
+import React, { useEffect, useState } from 'react'
+import './BoardContent.scss'
+import Column from '../Column/Column'
+import { initialData } from '../../actions/initialData'
+import { isEmpty } from 'lodash'
+import { mapOrder } from '../../untilities/sort'
+import { Container, Draggable } from 'react-smooth-dnd'
 
-function BoardContent(){
-    const [board,setBoard] = useState({})
-    const [columns,setColumns] = useState([])
+function BoardContent() {
+  const [board, setBoard] = useState({})
+  const [columns, setColumns] = useState([])
 
-    useEffect(()=>{
-        const boardDB = initialData.boards.find(board=>board.id==='board-1')
-        if(boardDB){
-            setBoard(boardDB)
+  useEffect(() => {
+    const boardDB = initialData.boards.find(board => board.id==='board-1')
+    if (boardDB) {
+      setBoard(boardDB)
 
-            mapOrder(boardDB.columns,boardDB.columnOrder,'id')
+      mapOrder(boardDB.columns, boardDB.columnOrder, 'id')
 
-            setColumns(boardDB.columns)
-        }
-    },[])
-    console.log(board)
-    if(isEmpty(board)){
-        return <div className="not-found">Board Not Found</div>
+      setColumns(boardDB.columns)
     }
+  }, [])
+  if (isEmpty(board)) {
+    return <div className="not-found">Board Not Found</div>
+  }
+  const onColumnDrop = (dropResult) => {
+    console.log(dropResult)
+  }
 
-    return(
-        <div className="board-content">
-            {columns.map((column,index)=><Column key={index} column={column}/>)}
-        </div>
-    )
+  return (
+    <div className="board-content">
+      <Container
+        orientation="horizontal"
+        onDrop={onColumnDrop}
+        dragHandleSelector=".column-drag-handle"
+        getChildPayload={index => columns[index]}
+        dropPlaceholder={{
+          animationDuration: 150,
+          showOnTop: true,
+          className: 'column-drop-preview'
+        }}
+      >
+        {columns.map((column, index) => (
+          <Draggable key={index}>
+            <Column column={column}/>
+          </Draggable>
+        ))}
+      </Container>
+    </div>
+  )
 }
 
 export default BoardContent
