cecd84ae00e8bdefed1cbc22fcd757b5eb3553d1
thanhbxdev <thanhbxdev@gmail.com>
2021-09-08 23:39:08 +0700

    CRUD API

--- src/actions/ApiCall/index.js
+++ src/actions/ApiCall/index.js
@@ -1,6 +1,22 @@
 import axios from 'axios'
+import { API_ROOT_CONFIRM } from '../../untilities/constants'
 
 export const fetchFullBoard = async (id) => {
-  const req = await axios.get(`http://localhost:8080/v1/boards/${id}`)
+  const req = await axios.get(`${API_ROOT_CONFIRM}/v1/boards/${id}`)
+  return req.data
+}
+
+export const createNewColumn = async (data) => {
+  const req = await axios.post(`${API_ROOT_CONFIRM}/v1/columns`, data)
+  return req.data
+}
+
+export const updateColumnAPI = async (id, data) => {
+  const req = await axios.put(`${API_ROOT_CONFIRM}/v1/columns/${id}`, data)
+  return req.data
+}
+
+export const createNewCard = async (data) => {
+  const req = await axios.post(`${API_ROOT_CONFIRM}/v1/cards`, data)
   return req.data
 }
