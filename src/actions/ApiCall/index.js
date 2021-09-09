0926db603396c49a0739c9ccde641b30167c83f5
thanhbxdev <thanhbxdev@gmail.com>
2021-09-09 18:19:53 +0700

    Drag Drop

--- src/actions/ApiCall/index.js
+++ src/actions/ApiCall/index.js
@@ -1,6 +1,11 @@
 import axios from 'axios'
 import { API_ROOT_CONFIRM } from '../../untilities/constants'
 
+export const updateBoard = async (id, data) => {
+  const req = await axios.put(`${API_ROOT_CONFIRM}/v1/boards/${id}`, data)
+  return req.data
+}
+
 export const fetchFullBoard = async (id) => {
   const req = await axios.get(`${API_ROOT_CONFIRM}/v1/boards/${id}`)
   return req.data
@@ -18,5 +23,9 @@ export const updateColumnAPI = async (id, data) => {
 
 export const createNewCard = async (data) => {
   const req = await axios.post(`${API_ROOT_CONFIRM}/v1/cards`, data)
+  return req.data
+}
+export const updateCardAPI = async (id, data) => {
+  const req = await axios.put(`${API_ROOT_CONFIRM}/v1/cards/${id}`, data)
   return req.data
 }
